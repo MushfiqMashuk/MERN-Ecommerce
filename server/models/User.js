d35cc96d289efc3b7a9c8c7f93833ec1f0a10aa5
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2021-10-31 00:39:57 +0600

    server commit

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,4 @@
-const { Schema } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
@@ -15,4 +15,4 @@ const userSchema = new Schema(
   }
 );
 
-module.exports = mongoose.model("Users", userSchema);
+module.exports = model("Users", userSchema);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,19 +3,26 @@ const User = require("../models/User");
 
 const router = express.Router();
 
+// Register a user
 router.post("/register", async (req, res) => {
-  const userModel = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  if (req.body.username && req.body.email && req.body.password) {
+    const userModel = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-  try {
-    const savedUser = await userModel.save();
-
-    res.status(200).json(savedUser);
-  } catch (err) {
-    res.status(5000).json(err);
+    try {
+      const savedUser = await userModel.save();
+      
+      res.status(200).json(savedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(500).json({
+      error: "Please fill-up all the fields!",
+    });
   }
 });
 
