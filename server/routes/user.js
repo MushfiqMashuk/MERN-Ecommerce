b49b3dcdd52388d2a18d18ef3ae2e8df927cb5dd
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2021-11-04 04:20:36 +0600

    server commit

--- server/routes/user.js
+++ server/routes/user.js
@@ -55,8 +55,9 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
 
 // Get all users
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
   try {
-    const users = await User.find();
+    const users = query ? await User.find().sort({_id: -1}).limit(5) : await User.find();
 
     res.status(200).json(users);
   } catch (err) {
