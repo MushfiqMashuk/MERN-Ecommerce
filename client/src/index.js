312bb0c8af283bad79070d40f4f6def294fa6b4c
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-03-23 19:10:12 +0600

    Redux Store Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
