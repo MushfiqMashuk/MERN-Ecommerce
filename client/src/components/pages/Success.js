0bce1418243599385d738fdc7472f22771819595
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-03-29 01:41:33 +0600

    Success Container

--- client/src/components/pages/Success.js
+++ client/src/components/pages/Success.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { privateRequest } from "../../handlers/requestMethods";
 import { deleteProduct } from "../../redux/cartSlice";
 
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flexdirection: column;
+  alignitems: center;
+  justifycontent: center;
+`;
 
 const Success = () => {
   const location = useLocation();
@@ -15,41 +24,40 @@ const Success = () => {
   const [orderId, setOrderId] = useState(null);
 
   useEffect(() => {
-    const createOrder = async () => {
-      try {
-        const response = await privateRequest.post("/order", {
-          userId: currentUser._id,
-          products: cart.products.map((item) => ({
-            productId: item._id,
-            quantity: item._quantity,
-          })),
-          amount: cart.total,
-          address: data.billing_details.address,
-        });
-        setOrderId(response.data._id);
-        dispatch(deleteProduct());
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    data && createOrder();
+    if (data) {
+      const createOrder = async () => {
+        try {
+          const response = await privateRequest.post("/order", {
+            userId: currentUser._id,
+            products: cart.products.map((item) => ({
+              productId: item._id,
+              quantity: item._quantity,
+            })),
+            amount: cart.total,
+            address: data.billing_details.address,
+          });
+          setOrderId(response.data._id);
+          dispatch(deleteProduct());
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      data && createOrder();
+    }
   }, [cart, data, currentUser]);
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
+    <Container>
       {orderId
         ? `Order has been created successfully. Your order number is ${orderId}`
         : `Successfull. Your order is being prepared...`}
-      <button style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
-    </div>
+
+      <Link to="/">
+        <button style={{ padding: 10, marginTop: 20, cursor: "pointer" }}>
+          Go to Homepage
+        </button>
+      </Link>
+    </Container>
   );
 };
 
